df7cbc2523c038a4bd460942806bc5ad9576dc5c
tassoman <tassoman@gmail.com>
2022-08-25 01:59:45 +0200

    updating intro video

--- src/pages/IndexDuePage.vue
+++ src/pages/IndexDuePage.vue
@@ -3,7 +3,7 @@
     <h2 class="text-h2 text-center">Welcome Homepage</h2>
     <p class="text-center">Website construction, please come back soon ...</p>
     <q-video
-      src="https://www.youtube.com/watch?v=J2eoBWZp64s"
+      src="https://www.youtube.com/embed/TrLrXsyfBhs"
       :ratio="560 / 315"
     />
   </q-page>
