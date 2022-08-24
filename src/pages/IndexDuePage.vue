9b859444d5a4dddc8e98d8996c4eee1988a567e5
tassoman <tassoman@gmail.com>
2022-08-25 01:40:19 +0200

    adding fancy homepage

---
+++ src/pages/IndexDuePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <h2 class="text-h2 text-center">Welcome Homepage</h2>
+    <p class="text-center">Website construction, please come back soon ...</p>
+    <q-video
+      src="https://www.youtube.com/watch?v=J2eoBWZp64s"
+      :ratio="560 / 315"
+    />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  // name: 'PageName'
+});
+</script>
