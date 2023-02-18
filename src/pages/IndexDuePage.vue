460c834b15f88d762f095c5c70cd0a69c0a9530c
Tassoman <tassoman@users.noreply.github.com>
2023-02-18 16:01:05 +0100

    Lello Kawasaki promo

--- src/pages/IndexDuePage.vue
+++ src/pages/IndexDuePage.vue
@@ -3,7 +3,7 @@
     <h2 class="text-h2 text-center">Welcome Homepage</h2>
     <p class="text-center">Website construction, please come back soon ...</p>
     <q-video
-      src="https://www.youtube.com/embed/TrLrXsyfBhs"
+      src="https://www.youtube.com/embed/Cln-rhxEC5M"
       :ratio="560 / 315"
     />
   </q-page>
@@ -14,4 +14,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   // name: 'PageName'
 });
+
+// https://youtu.be/Cln-rhxEC5M
+// https://www.youtube.com/embed/TrLrXsyfBhs
 </script>
